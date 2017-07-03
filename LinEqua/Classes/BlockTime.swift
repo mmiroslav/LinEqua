2c601cf4d33695bdb302d41f4299b98933f21d34
Miroslav Milivojevic <miroslav.milivojevic@bstorm.co>
2017-07-03 15:55:23 +0200

    Time calculations

---
+++ LinEqua/Classes/BlockTime.swift
@@ -0,0 +1,13 @@
+//
+//  BlockTime.swift
+//  Pods
+//
+//  Created by Miroslav Milivojevic on 7/3/17.
+//
+//
+
+import UIKit
+
+class BlockTime: NSObject {
+
+}
---
+++ LinEqua/Classes/Protocols.swift
@@ -0,0 +1,9 @@
+//
+//  Protocols.swift
+//  Pods
+//
+//  Created by Miroslav Milivojevic on 7/3/17.
+//
+//
+
+import Foundation
